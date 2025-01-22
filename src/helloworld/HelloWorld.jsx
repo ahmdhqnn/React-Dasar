7ce683f938a69dcdf45996052249b07795e31b68
ahmdhqnn <ahmadhaqinn@gmail.com>
2025-01-22 08:55:05 +0700

    Component

--- src/helloworld/HelloWorld.jsx
+++ src/helloworld/HelloWorld.jsx
@@ -1,8 +1,24 @@
 export default function HelloWorld() {
     return (
         <div>
+            <HeaderHelloWorld/>
+            <ParagraphHeloWorld/>
+        </div>
+    )
+}
+
+function HeaderHelloWorld() {
+    return (
+        <div>
             <h1>Hello World</h1>
         </div>
     )
 }
 
+function ParagraphHeloWorld() {
+    return (
+        <div>
+            <p>omaga gantengnya</p>
+        </div>
+    )
+}
