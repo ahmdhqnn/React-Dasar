692f4f80d8cb3ff72be41c002bd13584074d3b73
ahmdhqnn <ahmadhaqinn@gmail.com>
2025-01-23 19:28:10 +0700

    Collection component

--- src/todo/TodoList.jsx
+++ src/todo/TodoList.jsx
@@ -1,12 +1,42 @@
 import Todo from "./Todo.jsx"
 
 export default function TodoList() {
+
+    const data = [
+        {
+            id: 0,
+            text: "Learn Java",
+            isCompleted: true
+        },
+        {
+            id: 1,
+            text: "Learn JavaScript",
+            isCompleted: true
+        },
+        {
+            id: 2,
+            text: "Learn React",
+            isCompleted: true
+        },
+        {
+            id: 3,
+            text: "Learn AI",
+            isCompleted: true
+        },
+        {
+            id: 4,
+            text: "Learn Python",
+            isCompleted: false
+        },
+    ]
+
+    // const todos = data.map((todo) => <Todo key={todo.id} {...todo} />)
     return (
         <ul>
-            <Todo isCompleted={true} text="Learn JavaScript"/>
-            <Todo isCompleted={true} isDeleted={true} text="Learn Pyhton"/>
-            <Todo isCompleted={true} text="Learn ReactJs"/>
-            <Todo isCompleted={false} text="Learn AI"/>
+            {data.map((todo) => (
+
+                <Todo key={todo.id} {...todo} />
+            ))}
         </ul>
     )
 }
