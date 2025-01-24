f4aad2bab9d1ead151b8974a6a24d65c3f8864bf
ahmdhqnn <ahmadhaqinn@gmail.com>
2025-01-24 23:41:34 +0700

    Event handler

--- src/helloworld/main.jsx
+++ src/helloworld/main.jsx
@@ -4,15 +4,19 @@ import HelloWorld from "./HelloWorld.jsx";
 import Container from "./Container.jsx";
 import TodoList from "../todo/TodoList.jsx";
 import Table from "../table/Table.jsx";
+import AlertButton from "../button/AlertButton.jsx";
+import MyButton from "../button/MyButton.jsx";
 
 
 createRoot(document.getElementById("root"))
     .render(
         <StrictMode>
             <Container>
-                <HelloWorld/>
-                <TodoList/>
-                <Table/>
+                <HelloWorld />
+                <TodoList />
+                <Table />
+                <AlertButton text="click me" message="You click me" />
+                <MyButton text="Smash me" onSmash={() => alert("You smash me")} />
             </Container>
         </StrictMode>
     );
