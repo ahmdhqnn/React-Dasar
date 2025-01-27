54ba98f82773708aaf01bfdb2401a55dfb1f2e93
ahmdhqnn <ahmadhaqinn@gmail.com>
2025-01-27 22:10:20 +0700

    State

---
+++ src/form/Counter.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+export default function Counter() {
+    let [counter, setCounter] = useState(0);
+
+    return (
+        <div>
+            <div>
+                <button onClick={() => {
+                    setCounter(counter + 1); console.log(counter);
+                }}>Increment</button>
+            </div>
+
+            <h1>Counter : {counter}</h1>
+        </div>
+    )
+}
