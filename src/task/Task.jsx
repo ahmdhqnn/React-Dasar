d0b460b8a8e8a8d1af8a506bc6a3d02271a479d5
ahmdhqnn <ahmadhaqinn@gmail.com>
2025-02-07 21:35:25 +0700

    Sharing state

--- src/task/Task.jsx
+++ src/task/Task.jsx
@@ -5,16 +5,10 @@ import TaskList from "./TaskList.jsx";
 export default function Task() {
     const [items, setItems] = useImmer([]);
 
-    function handleOnSubmit(item) {
-        setItems((draft) => {
-            draft.push(item);
-        })
-    }
-
     return (
         <div>
 
-            <TaskForm onSubmit={handleOnSubmit} />
+            <TaskForm setItems={setItems} />
             <TaskList items={items} />
         </div>
     )
